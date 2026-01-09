c7e126fa3e0fdf467c0298dac2d488a0ecb2d72d
Jeden1102 <bum123@o2.pl>
2026-01-09 22:49:28 +0100

    master. Possible fix for mobile transition cards.

--- src/components/molecules/Card.vue
+++ src/components/molecules/Card.vue
@@ -3,7 +3,7 @@
     <FloatingLines
       :lines-gradient="['#41B883', '#34495E', '#41B883']"
       :enabled-waves="['top', 'bottom']"
-      class="!absolute top-0 left-0"
+      class="!absolute top-0 left-0 rounded-lg overflow-hidden"
       mix-blend-mode="hard-light"
     />
     <div class="relative z-10">
